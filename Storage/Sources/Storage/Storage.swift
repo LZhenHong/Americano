9be28ee16f1400b6f55c201c55358b95d3354cf5
Eden <levine5blue@gmail.com>
2024-05-12 23:38:24 +0800

    [UPDATE] SPM to remote.

--- Storage/Sources/Storage/Storage.swift
+++
@@ -1,5 +0,0 @@
-@attached(memberAttribute)
-public macro storage() = #externalMacro(module: "StorageMacros", type: "StorageMacro")
-
-@attached(peer)
-public macro nonstorage() = #externalMacro(module: "StorageMacros", type: "NonStorageMacro")
