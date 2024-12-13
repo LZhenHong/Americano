3e265e7bd2ff8b958ec9c8644a521d2c0542abb8
Eden <levine5blue@gmail.com>
2024-12-13 11:58:49 +0800

    [UPDATE] Use SwiftUI Slider.

--- Americano/Settings/Battery/BatterySlider.swift
+++ Americano/Settings/Battery/BatterySlider.swift
@@ -5,50 +5,7 @@
 //  Created by Eden on 2024/1/30.
 //
 
-import Cocoa
 import SwiftUI
-
-struct WrapSlider: NSViewRepresentable {
-    typealias NSViewType = NSSlider
-
-    let minValue: Int
-    let maxValue: Int
-    let numberOfTickMarks: Int
-    @Binding var currentValue: Int
-    @Binding var enabled: Bool
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    func makeNSView(context: Context) -> NSSlider {
-        let slider = NSSlider(target: context.coordinator,
-                              action: #selector(Coordinator.valueChanged))
-        slider.sliderType = .linear
-        slider.minValue = Double(minValue)
-        slider.maxValue = Double(maxValue)
-        slider.numberOfTickMarks = numberOfTickMarks
-        slider.allowsTickMarkValuesOnly = true
-        return slider
-    }
-
-    func updateNSView(_ nsView: NSSlider, context: Context) {
-        nsView.integerValue = currentValue
-        nsView.isEnabled = enabled
-    }
-
-    class Coordinator: NSObject {
-        var slider: WrapSlider
-
-        init(_ slider: WrapSlider) {
-            self.slider = slider
-        }
-
-        @objc func valueChanged(_ sender: NSSlider) {
-            slider.currentValue = sender.integerValue
-        }
-    }
-}
 
 struct BatterySlider: View {
     let minValue: Int
@@ -66,11 +23,20 @@ struct BatterySlider: View {
 
     var body: some View {
         VStack {
-            WrapSlider(minValue: minValue,
-                       maxValue: maxValue,
-                       numberOfTickMarks: numberOfTickMarks,
-                       currentValue: $currentValue,
-                       enabled: $enabled)
+            Slider(
+                value: Binding(
+                    get: {
+                        Double(currentValue)
+                    },
+                    set: {
+                        currentValue = Int($0)
+                    }
+                ),
+                in: Double(minValue) ... Double(maxValue),
+                step: 10
+            )
+            .disabled(!enabled)
+
             HStack {
                 Text("\(minValue)%")
                 Spacer()
